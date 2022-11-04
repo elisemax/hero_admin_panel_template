30b13de1698a29b6434ba2f770b03c13029c3af3
Max <max11cz@seznam.cz>
2022-11-04 14:46:57 +0100

    cdr

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import heroes from '../components/heroesList/heroesSlice';
+import filters from '../components/heroesFilters/filtersSlice';
+import {apiSlice} from '../api/apiSlice';
+
+const stringMiddleware = () => (next) => (action) => {
+    if(typeof action === 'string'){
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+const store = configureStore({
+    reducer: {
+        heroes,
+        filters,
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(stringMiddleware, apiSlice.middleware),
+    devTools: process.env.NODE_ENV !== 'production',
+});
+
+
+export default store;
