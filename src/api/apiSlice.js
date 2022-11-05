c32630792dad729dbf7deb46c1af29ca669f1262
Max <max11cz@seznam.cz>
2022-11-05 18:58:30 +0100

    fer

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -3,10 +3,27 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3001'}),
+    tagTypes: ['Heroes'],
     endpoints: (builder) => ({
         getHeroes: builder.query({
             query: () => '/heroes',
+            providesTags: ['Heroes']
+        }),
+        createHero: builder.mutation({
+            query: (newHero) => ({
+                url: '/heroes',
+                method: 'POST',
+                body: newHero
+            }),
+            invalidatesTags: ['Heroes']
+        }),
+        deleteHero: builder.mutation({
+            query: (id) => ({
+                url: `/heroes/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Heroes']
         })
     }),
 })
-export const {useGetHeroesQuery} = apiSlice;
+export const {useGetHeroesQuery, useCreateHeroMutation, useDeleteHeroMutation} = apiSlice;
--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -2,7 +2,9 @@ import {useHttp} from '../../hooks/http.hook';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-
+import {useCreateHeroMutation} from '../../api/apiSlice';
+import { selectAll } from '../heroesFilters/filtersSlice';
+import store from '../../store';
 import { heroCreated } from '../heroesList/heroesSlice';
 
 const HeroesAddForm = () => {
@@ -10,9 +12,11 @@ const HeroesAddForm = () => {
     const [heroDescr, setHeroDescr] = useState('');
     const [heroElement, setHeroElement] = useState('');
 
-    const {filters, filtersLoadingStatus} = useSelector(state => state.filters);
-    const dispatch = useDispatch();
-    const {request} = useHttp();
+    const [createHero, {isLoading}] = useCreateHeroMutation();
+
+
+    const { filtersLoadingStatus} = useSelector(state => state.filters);
+    const filters = selectAll(store.getState());
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
@@ -23,10 +27,7 @@ const HeroesAddForm = () => {
             element: heroElement
         }
 
-        request("http://localhost:3001/heroes", "POST", JSON.stringify(newHero))
-            .then(res => console.log(res, 'Отправка успешна'))
-            .then(dispatch(heroCreated(newHero)))
-            .catch(err => console.log(err));
+        createHero(newHero).unwrap();
 
         setHeroName('');
         setHeroDescr('');
@@ -34,7 +35,8 @@ const HeroesAddForm = () => {
     }
 
     const renderFilters = (filters, status) => {
-        if (status === "loading") {
+        console.log(filters);
+        if (status === 'loading') {
             return <option>Загрузка элементов</option>
         } else if (status === "error") {
             return <option>Ошибка загрузки</option>
